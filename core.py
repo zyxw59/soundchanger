7442752944101a5680b46ff37e1d4e540038bd8a
Sasha C F <sashacf@mit.edu>
2014-08-05 20:48:10 -0700

    changed stuff

--- core.py
+++ core.py
@@ -13,6 +13,6 @@ if __name__ == '__main__':
     print('Content-Type: text/html')
     print('')
     print(FILE_PATH + '/files/' + 'fluf')
-    f = open(FILE_PATH + '/files/fluf')
+    f = open(FILE_PATH + '/files/fluf', encoding='utf-8')
     r = f.read()
     print(reencode(f))
