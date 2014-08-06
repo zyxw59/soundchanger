5d6c3f5ff504f820d313ed59ec0d822a58591304
Sasha C F <sashacf@mit.edu>
2014-08-05 20:29:09 -0700

    changed stuff

--- core.py
+++ core.py
@@ -4,3 +4,12 @@ FILE_PATH = '/mit/sashacf/web_scripts/soundchanger'
 
 def reencode(s):
     return s.encode('ascii', 'xmlcharrefreplace').decode()
+
+if __name__ == '__main__':
+    print('Content-Type: text/html')
+    print('')
+    print(FILE_PATH + '/files/' + 'fluf')
+    try:
+        open(FILE_PATH + '/files/fluf')
+    except Exception as e:
+        print('noooo', type(e))
