9ff6f85dedcd09e36c710183ebcf182fa01976fa
Sasha C F <sashacf@mit.edu>
2014-08-05 22:39:11 -0700

    changed stuff

--- index.py
+++ index.py
@@ -26,7 +26,7 @@ fs = [\'''' + '\', \''.join(fs) + '''\'];
 numFiles = files.length;
 numPairs = 0;
 </script>
-<link href="main.css" rel="stylesheet" />
+<link href="min.css" rel="stylesheet" />
 <script src="main.js" ></script>
 </head>
 <body>
