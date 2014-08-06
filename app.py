817cf55dcc2e41e6156627aea8e423b42d228d49
Sasha C F <sashacf@mit.edu>
2014-08-05 18:40:26 -0700

    modified app.py

--- app.py
+++ app.py
@@ -9,6 +9,20 @@ cgitb.enable()
 print('Content-Type: text/html')
 print('')
 
-form = cgi.FieldStorage()
+form = cgi.FieldStorage(encoding='utf-8')
 
-print(form)
+start = {}
+end = {}
+
+for f in form:
+    if f[0] == 's':
+        start[f] = form[f].value
+    elif f[0] == 'e':
+        end[f] = form[f].value
+
+start = [v for k, v in sorted(start.items())]
+end = [v for k, v in sorted(end.items())]
+
+pairs = list(zip(start, end))
+
+print(pairs)
