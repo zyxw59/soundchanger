cf114dd63ba3aa0f897c42ea1cee733c0892643f
Sasha C F <sashacf@mit.edu>
2014-08-05 21:00:20 -0700

    changed stuff

--- app.py
+++ app.py
@@ -31,7 +31,4 @@ end = [v for k, v in sorted(end.items())]
 
 pairs = list(zip(start, end))
 
-try:
-    asc(word, pairs, pre=FILE_PATH)[0]
-except Exception as e:
-    print(e)
+print(reencode(asc(word, pairs, pre=FILE_PATH)[0]))
