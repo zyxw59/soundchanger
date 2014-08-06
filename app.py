3a66a85746555925bda5eb4a9fcc5418a127f595
Sasha C F <sashacf@mit.edu>
2014-08-06 13:22:03 -0700

    changed stuff

--- app.py
+++ app.py
@@ -38,8 +38,8 @@ print(reencode(word), pairs)
 
 word, db = asc(word, pairs, debug, FILE_PATH)
 try:
-    f = open(FILE_PATH + '/files/' + pairs[0][1], encoding='utf-8')
     print(FILE_PATH + '/files/' + pairs[0][1])
+    f = open(FILE_PATH + '/files/' + pairs[0][1], encoding='utf-8')
     print(reencode(f.read()))
 except Exception as e:
     print(e.args)
