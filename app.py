dc41e280107370a455a5c4b8123813b26896e9af
Sasha C F <sashacf@mit.edu>
2014-08-05 18:44:57 -0700

    changed app.py

--- app.py
+++ app.py
@@ -17,8 +17,12 @@ end = {}
 for f in form:
     if f[0] == 's':
         start[f] = form[f].value
+        if start[f] == ' ':
+            start[f] = ''
     elif f[0] == 'e':
         end[f] = form[f].value
+        if end[f] == ' ':
+            end[f] = ''
 
 start = [v for k, v in sorted(start.items())]
 end = [v for k, v in sorted(end.items())]
